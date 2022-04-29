0b4d62ede5f9721d35731684ba3d944e15a0ed06
Avi Saven <asaven@grammatech.com>
2022-04-29 13:01:04 -0400

    Fixed return value read analysis

--- tests/test_signature.py
+++ tests/test_signature.py
@@ -15,7 +15,7 @@ x:
 )
 def test_return_value_simple(result):
     """Test that return values are recovered correctly"""
-    result.assertContains("writes_unread_return_value", ("x", 0x4000))
+    result.assertContains("writes_direct_return_value", ("x", 0x4000))
     result.assertContains("writes_return_value", ("x", 0x4000))
 
 
@@ -39,11 +39,11 @@ br_done:
 def test_return_value_branch(result):
     """Test that return values are recovered correctly"""
     # Unreachable write to the return register
-    result.assertNotContains("writes_unread_return_value", ("x", 0x4000))
+    result.assertNotContains("writes_direct_return_value", ("x", 0x4000))
 
     # Reachable writes to the return register
-    result.assertContains("writes_unread_return_value", ("x", 0x4009))
-    result.assertContains("writes_unread_return_value", ("x", 0x4010))
+    result.assertContains("writes_direct_return_value", ("x", 0x4009))
+    result.assertContains("writes_direct_return_value", ("x", 0x4010))
     result.assertContains("writes_return_value", ("x", 0x4009))
     result.assertContains("writes_return_value", ("x", 0x4010))
 
@@ -69,14 +69,35 @@ def test_return_implicit(result):
     """Test that implicit return values are calculated correctly and that reads
     to them are detected as well
     """
-    result.printTable("reads_implicit_return_value")
     result.assertContains("writes_return_value", ("x", 0x4000))
     result.assertContains("call_graph", ("y", "x"))
-    result.assertNotContains("writes_unread_return_value", ("y", None))
+    result.assertNotContains("writes_direct_return_value", ("y", None))
     result.assertContains("may_pass_implicit_return_value", ("y", "x", 0x4000))
 
     # XXX: ddisasm appears to parse after the call as a separate function?
     result.assertContains(
-        "reads_implicit_return_value", ("z", "FUN_16401", 0x4011, "RAX")
+        "reads_return_value", ("z", "FUN_16401", 0x4011, "RAX")
     )
-    result.assertContains("writes_unread_return_value", ("FUN_16401", 0x4011))
+    result.assertContains("writes_direct_return_value", ("FUN_16401", 0x4011))
+
+
+@pytest.mark.commit
+@table_test(
+    """
+    x:
+    mov EAX, 1
+    ret
+
+    y:
+    call x
+    mov ebx, eax
+    add ebx, 1
+    mov eax, ebx
+    ret
+    """,
+    gtirb.Module.ISA.X64,
+    functions=["x", "y"],
+)
+def test_return_reads_direct(result):
+    """Test that return value reads are calculated correctly"""
+    result.assertContains("reads_return_value", ("y", "x", 0x400B, "RAX"))
